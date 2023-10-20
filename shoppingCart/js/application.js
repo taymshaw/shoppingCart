06490286147625fdfa658c39e1c0f92ef35cafdd
Taylor Shaw <taylorshaw@Taylors-MacBook-Air.local>
2023-10-20 09:12:07 -0500

    updated js to remove from total cost

--- shoppingCart/js/application.js
+++ shoppingCart/js/application.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
     // remove an item
     $('tbody').on('click', '.btn.remove', function () {
         $(this).closest('tr').remove();
+        updateFinalTotal();
     });
 
         // sub total of each line item
